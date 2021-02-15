e8b443e261b6110bead77aa62625360ff83f3df3
Anita-ihuman <charlesanita403@gmail.com>
2021-02-16 00:25:42 +0100

    Fixed the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,50 +7,33 @@ const Footer = () => {
       <footer>
         <div className={styles.Third}>
           <div className={styles.text}>
-            <h3>WHO WE ARE</h3>
-            <p>About </p>
-            <p> Team</p>
-            <p>Work With Us</p>
+            <div>
+              <img src="Logo.png" className={styles.textimg} />
+              <p>
+                Get out there & discover your next slope, mountain &
+                destination!
+              </p>
+            </div>
+            <div className={styles.copy}>
+              Copyright 2019 MNTN, Inc. Terms & Privacy
+            </div>
           </div>
-          <div className={styles.text}>
-            <h3>Woocommerce</h3>
-            <p>Features </p>
-            <p>Payments</p>
-            <p>Marketing</p>
-            <p>Shipping</p>
-            <p>Extension Store</p>
-            <p>eCommerce blog</p>
-            <p>Development blog</p>
-            <p>Ideas board </p>
-            <p>Mobile App </p>
-            <p>Style Guide </p>
-            <p> Email Newsletter</p>
-          </div>
+          <div className={styles.text2}>
+            <div className={styles.text}>
+              <h3>More on The Blog</h3>
+              <p>About MNTN</p>
+              <p>Contributors & Writers</p>
+              <p>Write For Us</p>
+              <p>Contact Us</p>
+              <p>Privacy Policy</p>
+            </div>
 
-          <div className={styles.text}>
-            <h3>OTHER PRODUCTS</h3>
-            <p>Storefront </p>
-            <p>WooSlider </p>
-            <p>Sensei </p>
-            <p>Sensei Extensions</p>
-          </div>
-          <div className={styles.text}>
-            <h3>SUPPORT</h3>
-            <p> Documentation</p>
-            <p> Customizations </p>
-            <p>Support Policy</p>
-            <p> Contact </p>
-            <p>COVID-19 Resources </p>
-            <p>Privacy Notice for </p>
-            <p>California Users</p>
-          </div>
-          <div className={styles.text}>
-            <h3>WE RECOMMEND</h3>
-            <p>WooExperts</p>
-            <p>Hosting Solutions</p>
-            <p>Pre-sales FAQ</p>
-            <p> Success Stories</p>
-            <p> Design Feedback Group</p>
+            <div className={styles.text1 }>
+              <h3>More on MNTN</h3>
+              <p>The Team </p>
+              <p>Jobs </p>
+              <p>Press </p>
+            </div>
           </div>
         </div>
       </footer>
