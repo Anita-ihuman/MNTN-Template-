b511542c949ce2fb3b7e6d6a8358eaeb015629da
Anita-ihuman <charlesanita403@gmail.com>
2021-02-15 12:11:54 +0100

    Working on the navbar

---
+++ components/nav.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import styles from "../styles/nav.module.scss";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className={styles.nav}>
+        <div className={styles.header}>
+          <div className={styles.brand}>
+            <img src="/Logo.png" alt="Logo" color="black" />
+          </div>
+
+          <ul className={styles.menu}>
+            <li className={styles.link}>
+              <a href="/">Equipment</a>
+            </li>
+            <li className={styles.link}>
+              <a href="/">About us</a>
+            </li>
+            <li className={styles.link}>
+              <a href="/">Blog</a>
+            </li>
+          </ul>
+
+          <div className={styles.side}>
+            <a href="/">
+              <img src="/cart.png" className={styles.search} color="black" />
+            </a>
+            <div className={styles.btn}>
+              <span>Account</span>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
