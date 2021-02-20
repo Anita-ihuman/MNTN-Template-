aeef071f49cd755168397d035ce57cacab3cbd91
Anita-ihuman <charlesanita403@gmail.com>
2021-02-20 23:50:07 +0100

    importing font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,13 @@ export default class CustomDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,300&display=swap"
             rel="stylesheet"
           />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Khula:wght@300&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet"/>
         </Head>
 
         <body>
